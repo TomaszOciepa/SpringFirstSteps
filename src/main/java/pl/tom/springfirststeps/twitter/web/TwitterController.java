b0c3a3885d731088dcec4bf57bb26dc362c76f6b
tomaszociepa <tom.ociepa@gmail.com>
2019-10-11 12:38:33 +0200

    getting path parameters

--- src/main/java/pl/tom/springfirststeps/twitter/web/TwitterController.java
+++ src/main/java/pl/tom/springfirststeps/twitter/web/TwitterController.java
@@ -4,6 +4,7 @@ package pl.tom.springfirststeps.twitter.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,17 +20,27 @@ public class TwitterController {
     @Autowired
     private TwitterRepository twitterRepository;
 
-
     @RequestMapping(method = RequestMethod.GET)
-    public String twitter(Model model, @RequestParam(value = "id", defaultValue = "0") int id){
-
-        if (id == 0){
-            model.addAttribute("twittList", twitterRepository.getTwitterList());
-        }else{
-            model.addAttribute("twittList", twitterRepository.getTwitterList(id));
-        }
+    public String twitter(Model model){
+        model.addAttribute("twittList", twitterRepository.getTwitterList());
         return "twitter";
     }
 
+//    @RequestMapping(method = RequestMethod.GET)
+//    public String twitter(Model model, @RequestParam(value = "id", defaultValue = "0") int id){
+//
+//        if (id == 0){
+//            model.addAttribute("twittList", twitterRepository.getTwitterList());
+//        }else{
+//            model.addAttribute("twittList", twitterRepository.getTwitterList(id));
+//        }
+//        return "twitter";
+//    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{twitterId}")
+    public String twitter(Model model, @PathVariable int twitterId){
+        model.addAttribute("twittList", twitterRepository.getTwitterList(twitterId));
+        return "twitter";
+    }
 
 }
